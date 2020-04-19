e40544ebd8879a4b27fc1a5f350740335149fae3
Михаил Красильников <m.krasilnikov@webbankir.com>
2020-04-19 14:20:37 +0300

    feat: support for PSR-16 simple cache in ClientRaw

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@
 
 - Added support for setting [PSR-18](https://www.php-fig.org/psr/psr-18/) HTTP client in
   `ClientRaw`.
+- Added support for [PSR-16](https://www.php-fig.org/psr/psr-16/) simple cache in `ClientRaw`.
 
 
 ## [1.1.0](https://github.com/badoo/jira-client/compare/v1.0.0...v1.1.0) (2020-02-04)
---
+++ src/Cache/NullCache.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Badoo\Jira\Cache;
+
+use Psr\SimpleCache\CacheInterface;
+
+/**
+ * Cache that doesn't cache anything.
+ *
+ * Used as a default cache to simplify cache related logic.
+ *
+ * @internal
+ */
+class NullCache implements CacheInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function clear()
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete($key)
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deleteMultiple($keys)
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get($key, $default = null)
+    {
+        return $default;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function has($key)
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function set($key, $value, $ttl = null)
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setMultiple($values, $ttl = null)
+    {
+        return true;
+    }
+}
